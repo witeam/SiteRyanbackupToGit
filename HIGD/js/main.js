196529ef4f15b22526bb4291cbfc7b641f64ee2c
witeam <ronald.danipog@transcosmos.com.ph>
2015-07-15 17:58:16 +0800

    HIGD

---
+++ HIGD/js/main.js
@@ -0,0 +1,24 @@
+﻿var toggleSearch = 0;
+
+$(document).ready(function() {
+	$('#gnavArea li:last-child').click(function() {
+		toggleSearchBar();
+	});
+});
+
+function toggleSearchBar() {
+	if(toggleSearch == 0) {
+		$('#headerArea form').css('display','block');
+		$('#headerArea form').animate({
+			opacity: 1
+		}, 300);
+		toggleSearch = 1;
+	} else {
+		$('#headerArea form').animate({
+			opacity: 0
+		}, 300, function() {
+			$('#headerArea form').css('display','none');
+		});
+		toggleSearch = 0;
+	}
+}
