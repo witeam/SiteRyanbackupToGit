b473ed6986f997ecf63ae7e68603b136ede7a9ac
witeam <ronald.danipog@transcosmos.com.ph>
2015-07-16 17:21:47 +0800

    TKB

---
+++ TKB/js/main.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function() {
+	$("#nav .navbar-nav li").click(function() {
+		var idx = $(this).data("name");
+    $('html, body').animate({
+      scrollTop: $("#"+idx).offset().top
+    }, 750);
+});
+});
